ef298528b9537ef51285420b879032f336615ead
nabin <budhathokinabin700@gmail.com>
2026-09-04 07:56:57 +0545

    add option

--- Quize..cpp
+++ Quize..cpp
@@ -2,9 +2,33 @@
 #include<iostream>
 #include<string>
 using namespace std;
+void option(int i){
+    int j;
+    string str2[5][4]={
+        {"a) dang","b) nepaljung","c) kapilvastu","d)kathmandu"},
+    {"a) kp","b) harka","c) balen", "d) parchandra"},
+    {"a) cat","b) cow","c)dog","d) horse"},
+    {"a) madav","b) deuba","c)bhimsen thapa","janga bahadur"},
+    {"a) 8848.86","b) 8908.90","c) 79087","d) 76838"},
+};
+  
+        for( j=0;j<4;j++){
+            cout<<str2[i][j]<<" ";
+
+        }
+        cout<<endl;
+    
+    
+
+    
+    
+    
+
+}
 int main(){
     int i;
-    string str[5]={"what is the capital city of  nepal ?",
+
+    string str1[5]={"what is the capital city of  nepal ?",
         "who is the current prime minister of nepal ?",
         "what is the national animal of nepal ?",
         "who is the first minister of nepal ?",
@@ -12,9 +36,17 @@ int main(){
 
 
     };
+     
+    cout<<"Quize question"<<endl;
+    
     for(i=0;i<5;i++){
-        cout<<(1+i)<<"\t"<<str[i]<<endl;
+        cout<<(1+i)<<"\t"<<str1[i]<<endl;
+        option(i);
+        
+        
     }
+
+  
     return 0;
 
 }
