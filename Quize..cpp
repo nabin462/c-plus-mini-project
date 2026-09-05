213c63f881e2a61169923d9b1cbbe7ae887c0f8a
nabin <budhathokinabin700@gmail.com>
2026-09-05 07:53:20 +0545

    complete the all quize relate problem

--- Quize..cpp
+++ Quize..cpp
@@ -2,13 +2,15 @@
 #include<iostream>
 #include<string>
 using namespace std;
+ string ans;
+ int score=0,wrong=0,righ=0;
 void option(int i){
     int j;
     string str2[5][4]={
         {"a) dang","b) nepaljung","c) kapilvastu","d)kathmandu"},
     {"a) kp","b) harka","c) balen", "d) parchandra"},
     {"a) cat","b) cow","c)dog","d) horse"},
-    {"a) madav","b) deuba","c)bhimsen thapa","janga bahadur"},
+    {"a) madav","b) deuba","c)bhimsen_thapa","d)janga_bahadur"},
     {"a) 8848.86","b) 8908.90","c) 79087","d) 76838"},
 };
   
@@ -18,15 +20,31 @@ void option(int i){
         }
         cout<<endl;
     
-    
+ 
 
-    
-    
-    
+}
 
+void correct(int i){
+    
+    string str3[5]={
+        "kathmandu",
+        "balen",
+        "cow",
+        "bhimsen thapa",
+        "8848.86",
+    };
+    if(str3[i]==ans)
+    {
+      score+=2;
+      righ+=1;
+    }
+    else{
+        wrong+=1;
+    }
 }
 int main(){
     int i;
+   
 
     string str1[5]={"what is the capital city of  nepal ?",
         "who is the current prime minister of nepal ?",
@@ -42,9 +60,16 @@ int main(){
     for(i=0;i<5;i++){
         cout<<(1+i)<<"\t"<<str1[i]<<endl;
         option(i);
+        cout<<"enter correct option"<<endl;
+        cin>>ans;
+        correct(i);
         
         
     }
+    cout<<"Total score="<<score<<endl;
+    cout<<"Right ="<<righ<<endl;
+    cout<<"wrong ="<<wrong<<endl;
+    
 
   
     return 0;
